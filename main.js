0b1702684567f342bb51fdd869d1fb1e1b9150e8
amaralthayla <amaral.thayla@escola.pr.gov.br>
2026-06-08 11:54:25 -0300

    lçmlç

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoess = document.querySelectorAll(".botao")
+const textos = document.querySelector(".aba-conteudo")
+
+for (let i = 0; < botoess.length; i++){
+    botoes[i].onclick =  () => {
+        for (let j = 0; j < botoes.length; j++) {
+        }
+    }
+}
